181027945edb44a668b74c676bdb0da070af63f8
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-13 00:32:03 -0300

    at2

--- Tp-4/a4.py
+++ Tp-4/a4.py
@@ -3,8 +3,8 @@ t = turtle.Turtle()
 
 t.color('red')
 t.speed(300)
-t.fillcolor('green')
-t.pencolor('black')
+t.fillcolor('blue')
+t.pencolor('red')
 
 for i in range(200):
   t.begin_fill()
@@ -12,3 +12,4 @@ for i in range(200):
   t.left(170)
   t.forward(300 - i)
   t.end_fill()
+turtle.done()
