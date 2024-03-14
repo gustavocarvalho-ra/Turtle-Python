7398e0850918c0d1977bd51e73b1a22be3510ca1
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-14 15:58:43 -0300

    at2

--- Tp-6/a6.py
+++ Tp-6/a6.py
@@ -4,3 +4,12 @@ bgcolor('black')
 speed(0)
 pensize(3)
 h = 0
+for i in range (300):
+  c=cs.hsv_to_rgb(h,1,1)
+  pencolor(c)
+  h+=0.05
+  right(i)
+  circle(50,i)
+  forward(i)
+  left(91)
+done()
