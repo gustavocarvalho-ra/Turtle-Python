36ed1fb3a666f6e93ff3524322297259ab549771
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-16 22:32:35 -0300

    at3

--- Tp-5/a5.py
+++ Tp-5/a5.py
@@ -1,17 +1,16 @@
 from turtle import *
-import colorsys as cs
-
-speed(0)
-pensize(3)
+import colorsys
 bgcolor('black')
+speed(0)
 h = 0
 
-for i in range(300):
-  c = cs.hls_to_rgb(h,1,1)
+for i in range(200):
+  c = colorsys.hls_to_rgb(h, 1, 1)
   pencolor(c)
-  h += 0.005
+  h += 0.05
   circle(5 - i, 100)
-  lt(80)
+  left(80)
   circle(5 - i,100)
-  rt(100)
-done()
+  right(100)
+  pensize(2)
+done() 
