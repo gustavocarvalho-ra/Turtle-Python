bee1acda30e66d751ba0d685d1601ea19adede42
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-17 22:10:08 -0300

    at3

--- Tp-10/a10.py
+++ Tp-10/a10.py
@@ -8,3 +8,15 @@ t.left(90)
 t.backward(100)
 t.speed(200)
 t.shape('turtle')
+
+def tree(i):
+  if i < 10:
+    return
+  else:
+    t.forward(i)
+    t.color('orange')
+    t.circle(2)
+    t.color('brown')
+    t.left(30)
+    tree(3 * i/4)
+    t.right(60)
