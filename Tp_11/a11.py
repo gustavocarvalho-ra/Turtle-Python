31b193c274442e8b769f5b5d53776ca2ef0bf947
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-21 23:39:44 -0300

    at3

--- Tp_11/a11.py
+++ Tp_11/a11.py
@@ -2,9 +2,13 @@ import turtle
 t = turtle.Turtle()
 s = turtle.Screen()
 s.bgcolor('black')
-t.pencolor('grenn')
+t.pencolor('red')
 a = 0
 b = 0
+t.speed(0)
+t.penup()
+t.goto(0, 200)
+t.pendown()
 
 while True:
   t.forward(a)
@@ -13,4 +17,4 @@ while True:
   b += 1
   if b ==210:
     break
-  
+  t.hideturtle()
