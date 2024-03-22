4adc0d3fb33a249bf619b9a5c53ac898dc023cb5
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-21 23:35:04 -0300

    at2

--- Tp_11/a11.py
+++ Tp_11/a11.py
@@ -3,3 +3,14 @@ t = turtle.Turtle()
 s = turtle.Screen()
 s.bgcolor('black')
 t.pencolor('grenn')
+a = 0
+b = 0
+
+while True:
+  t.forward(a)
+  t.right(b)
+  a += 3
+  b += 1
+  if b ==210:
+    break
+  
