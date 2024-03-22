27aa6d9753d40182a154aa9810a6e08c92a4ee89
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-22 00:07:19 -0300

    at1

--- Tp_12/a12.py
+++ Tp_12/a12.py
@@ -2,21 +2,26 @@ import turtle
 from turtle import *
 
 turtle.title('rainbow')
-speed(5)
+speed(50)
 bgcolor('black')
-r,g,b = 255, 0, 0
+r,g,b=255,0,0
 
 for i in range(255*2):
   colormode(255)
-  if i <255//3:
+  if i<255//3:
     g+=3
-  elif i <255*2//3:
+  elif i<255*2//3:
     r-=3
-  elif i <255:
+  elif i<255:
     b+=3
-  elif i <255*4//3:
+  elif i<255*4//3:
     g-=3
-  elif i <255*5//3:
+  elif i<255*5//3:
     r+=3
   else:
-    
+    b-=3
+
+  forward(50+i)
+  right(91)
+  pencolor(r,g,b)
+turtle.done()
