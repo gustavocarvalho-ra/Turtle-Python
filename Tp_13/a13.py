6cb6e245b8c8dd44b9f0014b98e8960186cf324b
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-22 00:15:22 -0300

    at2

--- Tp_13/a13.py
+++ Tp_13/a13.py
@@ -7,4 +7,11 @@ n = 70
 h = 0
 
 for i in range(360):
-  
+  c = colorsys.hsv_to_rgb(h, 1, 0.8)
+  h += 1/n
+  t.color(c)
+  t.left(1)
+  for j in range(2):
+    t.left(2)
+    t.circle(100)
+turtle.done()
