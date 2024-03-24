a2581dbb62a89c9ef14e74a811c49ac8dfe29fa1
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-24 02:12:14 -0300

    at1

--- Tp_16/a16.py
+++ Tp_16/a16.py
@@ -4,4 +4,8 @@ turtle.bgcolor('black')
 colors = ['red', 'yellow', 'green', 'purple', 'blue', 'orange']
 
 for i in range(210):
-  
+  t.pencolor(colors[i%6])
+  t.width(i/100+1)
+  t.forward(i)
+  t.left(59)
+turtle.done()
