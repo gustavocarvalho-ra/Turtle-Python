867d70167d83afa8a34ec0c796981c34dec66ee0
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-24 02:18:03 -0300

    at1

--- Tp_17/a17.py
+++ Tp_17/a17.py
@@ -11,3 +11,17 @@ speed(0)
 def line(i):
   for c in range(i):
     forward(c)
+    
+for i in range(200):
+  width(3)
+  color('red')
+  left(0)
+  forward(5)
+  circle(i)
+  width(3)
+  right(45)
+  color('orange')
+  forward(2)
+  circle(i)
+  
+done()
